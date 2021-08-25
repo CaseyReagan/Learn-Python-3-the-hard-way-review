7f2eb2ee92db92384a4b73d25e9ab3f0cc40fd20
Casey Reagan <caseyreagan0@gmail.com>
2021-08-25 15:07:53 +0800

    also useless system variable input, but a interesting input format with>

---
+++ ex14.py
@@ -0,0 +1,21 @@
+from sys import argv
+
+script, user_name = argv		#记得从windows命令行运行这个程序，赋予程序系统参数
+prompt = '>'
+
+print(f"Hi {user_name}, I'm the {script} script.")
+print("I'd like to ask you a few questions.")
+print(f"Do you like me {user_name}?")
+likes = input(prompt)
+
+print(f"Where do you live {user_name}?")
+lives = input(prompt)
+
+print(f"What kind of computer do you have?")
+computer = input(prompt)
+
+print(f"""
+Alright, so you said {likes} about liking me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice.
+""")
