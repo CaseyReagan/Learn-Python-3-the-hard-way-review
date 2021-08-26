4a1b8033c4c788e7199382952ba0bf9e40c20b50
Casey Reagan <caseyreagan0@gmail.com>
2021-08-26 15:30:47 +0800

    File operation in Python.truncate() and write() function
    be careful, write function won't add "\n" automatically in the end of the line. Have to add one manully.

---
+++ ex16.py
@@ -0,0 +1,34 @@
+from sys import argv
+
+script, filename = argv
+
+print(f"We're going to erase {filename}.")
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you do want that, hit RETURN.")
+
+input("?")
+
+print("Opening the file...")
+target = open(filename, 'w')
+
+print("Truncating the file. Goodbye!")
+target.truncate()			# truncate() 方法用于截断文件，如果指定了可选参数 size，则表示截断文件为 size 个字符。 如果没有指定 size，则从当前位置起截断；截断之后 size 后面的所有字符被删除。
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
+#line4 = line1 +'\n'+ line2 +'\n'+ line3
+
+print("I'm going to write these to the file.")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print("And finally, we close it.")
+target.close()
