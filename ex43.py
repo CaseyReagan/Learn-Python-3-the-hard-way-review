958f7e3e7e5f3d7f4068ac9623ab2cc907e6bc04
Casey Reagan <caseyreagan0@gmail.com>
2021-08-31 19:05:57 +0800

    write the details to the ex43_classes to realize the game

---
+++ ex43.py
@@ -0,0 +1,62 @@
+from sys import exit
+from random import randint
+from textwrap import dedent
+
+
+class Scene(object):
+
+	def enter(self):
+		print("This scene is not yet configured.")
+		print("Subclass it and implement enter().")
+		exit(1)
+
+class Engine(object):
+
+	def __init__(self, scene_map):
+		self.scene_map = scene_map
+
+	def play(self):
+		current_scene = self.scene_map.opening_scene()
+		last_scene = self.scene_map.next_scene('finished')
+
+		while current_scene != last_scene:
+			next_scene_name = current_scene.enter()
+			current_scene = self.scene_map.next_scene(next_scene_name)
+
+		# be sure to print out the last scene
+		current_scene.enter()
+
+class Death(Scene):
+
+	def enter(self):
+		pass
+
+class CentralCorridor(Scene):
+
+	def enter(self):
+		pass
+
+class LaserWeaponArmory(Scene):
+
+	def enter(self):
+		pass
+
+class EscapePod(Scene):
+
+	def enter(self):
+		pass
+
+class Map(object):
+
+	def __init__(self, start_scene):
+		pass
+
+	def next_scene(self, scene_name):
+		pass
+
+	def opening_scene(self):
+		pass
+
+a_map = Map('central_corridor')
+a_game = Engine(a_map)
+a_game.play()		
