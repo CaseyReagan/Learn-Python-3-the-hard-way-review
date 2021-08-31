00688561cd9ea6fd5a94604d70f45017f093c495
Casey Reagan <caseyreagan0@gmail.com>
2021-08-31 19:24:36 +0800

    update the Death class

--- ex43.py
+++ ex43.py
@@ -2,7 +2,6 @@ from sys import exit
 from random import randint
 from textwrap import dedent
 
-
 class Scene(object):
 
 	def enter(self):
@@ -28,8 +27,19 @@ class Engine(object):
 
 class Death(Scene):
 
+	quips = [
+			"You died. You kinda suck at this.",
+			"Your Mom would be proud...if she were smarter.",
+			"Such a luser.",
+			"I have a small puppy that's better at this.",
+			"You're worse than your Dad's jokes."
+
+		]
+
 	def enter(self):
-		pass
+		# 这里是从上面的quips列表里随机打印其中一条
+		print(Death.quips[randint(0, len(self.quips)-1)])
+		exit(1)
 
 class CentralCorridor(Scene):
 
