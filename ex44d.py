3f22e313df4a3a36e6b557ebd71eeb7b79b08954
Casey Reagan <caseyreagan0@gmail.com>
2021-09-01 15:21:16 +0800

    abc three file combine

---
+++ ex44d.py
@@ -0,0 +1,31 @@
+class Parent(object):
+	def override(self):
+		print("PARENT override()")
+
+	def implicit(self):
+		print("PARENT implicit()")
+
+	def altered(self):
+		print("PARENT altered()")
+
+class Child(Parent):
+
+	def override(self):
+		print("CHILD override()")
+
+	def altered(self):
+		print("CHILD, Before PARENT altered()")
+		super(Child,self).altered()
+		print("CHILD, After PARENT altered()")
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
+
+dad.altered()
+son.altered()
