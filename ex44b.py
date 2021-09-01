23b69d710b8c9439e2afa6281e8fe733287de556
Casey Reagan <caseyreagan0@gmail.com>
2021-09-01 13:47:25 +0800

    override the function in subclass which comes from base class

---
+++ ex44b.py
@@ -0,0 +1,18 @@
+## Override Explicitly
+class Parent(object):
+
+	def override(self):
+		print("Parent override()")
+
+class Child(Parent):
+
+	def override(self):
+		print("CHILD override()")
+
+dad = Parent()
+son = Child()
+
+dad.override()
+son.override()
+	
+		
