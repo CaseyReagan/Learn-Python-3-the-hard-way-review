88d70933480f1d1c0e08863fde111683468c62d6
Casey Reagan <caseyreagan0@gmail.com>
2021-09-02 16:16:10 +0800

    copy the python project skeleton to a new project ex47 & change details

---
+++ ex47/setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+config = {
+	'description' : 'ex47',
+	'author': 'Reagan',
+	'url': 'URL to get it at.',
+	'download_url': 'where to download it.',
+	'author_email': 'caseyreagan@163.com',
+	'version': '0.1',
+	'install_requires': ['nose'],
+	'packages': ['ex47'],
+	'scripts': [],
+	'name': 'Projectname'
+}
+
+setup(**config)
