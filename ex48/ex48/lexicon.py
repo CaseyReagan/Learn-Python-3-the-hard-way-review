7ca4e0b0c7c6318582921283d3ee93e36f9fc32c
Casey Reagan <caseyreagan0@gmail.com>
2021-09-06 13:51:40 +0800

    Finish the basic skeleton of texicon scan function

--- ex48/ex48/lexicon.py
+++ ex48/ex48/lexicon.py
@@ -1,2 +1,31 @@
-def scan():
-	pass
+WORD_TYPE = {
+	"north" : "direction",
+
+}
+
+def convert_number(s):
+	try:
+		return int(s)
+	except ValueError:
+		return None
+
+def scan(sentence):
+	words = sentence.split()
+	result = []
+
+	for word in words:
+		word_type = WORD_TYPE.get(word)
+
+		if word_type == None:
+			# it might be a number, so try converting
+			number = convert_number(word)
+
+			if number != None:
+				result.append(('number',number))
+			else:
+				result.append(('error'),word)
+
+		else:
+			result.append((word_type,word))
+
+	return result
