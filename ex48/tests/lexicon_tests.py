b5a6367c13a84efe97a1412f3a77343999f8b4ba
Casey Reagan <caseyreagan0@gmail.com>
2021-09-06 13:53:28 +0800

    follow the instruction of the book to add tests line by line

--- ex48/tests/lexicon_tests.py
+++ ex48/tests/lexicon_tests.py
@@ -36,4 +36,4 @@ from nose.tools import *
 from ex48 import lexicon
 
 def test_directions():
-	assert_equal(lexicon.scan("north"),[('direction'),('north')])
+	assert_equal(lexicon.scan("north"),[('direction','north')])
