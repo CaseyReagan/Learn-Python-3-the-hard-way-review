d7da70744f3a520d89dc139c107167314f7fe292
Casey Reagan <caseyreagan0@gmail.com>
2021-09-07 17:27:13 +0800

    Finish the parser subject and parser sentence

--- ex48/ex48/parser.py
+++ ex48/ex48/parser.py
@@ -50,6 +50,7 @@ def parse_verb(word_list):
 
 # 原理与上面相同，跳过所有的stop单词，先窥探，然后基于内容决定句子是否正确
 # 有两个if条件的原因是，宾语里既可能是名词也可能是方向词
+
 def parse_object(word_list):
 	skip(word_list, 'stop')
 	next_word = peek(word_list)
@@ -60,3 +61,22 @@ def parse_object(word_list):
 		return match(word_list, 'direction')
 	else:
 		raise ParserError("Expected a noun or direction next.")
+
+def parse_subject(word_list):
+	skip(word_list, 'stop')
+	next_word = peek(word_list)
+
+	if next_word == 'noun':
+		return match(word_list, 'noun')
+	elif next_word == 'verb':
+		return ('noun', 'player')
+	else:
+		raise ParserError("Expected a verb next.")
+
+## 最终句子的解释器
+def parse_sentence(word_list):
+	subj = parse_subject(word_list)
+	verb = parse_verb(word_list)
+	obj = parse_object(word_list)
+
+	return Sentence(subj, verb, obj)
