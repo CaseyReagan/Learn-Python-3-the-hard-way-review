428f17e7971c665edb3a8c9df7c6ba8cf5a03641
Casey Reagan <caseyreagan0@gmail.com>
2021-09-07 17:58:54 +0800

    Add many explaination comments of the code

--- ex48/ex48/parser.py
+++ ex48/ex48/parser.py
@@ -14,6 +14,7 @@ class Sentence(object):
 def peek(word_list):
 	if word_list:
 		word = word_list[0]
+## peek这里实际返回的是下一个单词的类型，比如verb，noun
 		return word[0]
 	else:
 		return None
@@ -24,6 +25,7 @@ def match(word_list, expecting):
 	if word_list:
 		word = word_list.pop(0)
 
+## 判断当前单词的type是不是我们想要的类型，如果是的话，就将这对词组返回
 		if word[0] == expecting:
 			return word
 		else:
@@ -34,6 +36,7 @@ def match(word_list, expecting):
 
 ## skip 不只跳过一个单词，它会跳过所有它所找到的那个类型的单词。
 def skip(word_list, word_type):
+## 当单词的类型与我们想要跳过的单词一样的时候，就进行（跳过）到下一个单词，如果不一样就退出循环
 	while peek(word_list) == word_type:
 		match(word_list, word_type)
 
@@ -80,3 +83,5 @@ def parse_sentence(word_list):
 	obj = parse_object(word_list)
 
 	return Sentence(subj, verb, obj)
+
+## 测试举例x = parse_sentence([('verb', 'run'), ('direction', 'north')])
