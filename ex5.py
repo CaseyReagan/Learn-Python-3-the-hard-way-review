9f8ee42f9ff87fdf791ae1571a25d5c5cab61be4
Casey Reagan <caseyreagan0@gmail.com>
2021-09-16 23:49:47 +0800

    add my practise

--- ex5.py
+++ ex5.py
@@ -17,3 +17,9 @@ print(f"His teeth are usually {my_teeth} depending on the coffee.")
 total = my_age + my_height + my_weight
 print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
 
+## my code for practise
+
+print(my_name)
+print("my name is:",my_name)
+print(f"his name is {my_name}")
+
